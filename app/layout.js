c71ee06166d426bbe8b03d75a5f909f7804a6f54
AlbertoGallardoDev <alberto.fullstackdeveloper@gmail.com>
2024-10-24 00:08:01 +0200

    Google analitycs | SEO google

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
 import localFont from "next/font/local";
+import Script from 'next/script';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 
@@ -17,14 +18,30 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "Elite Sports Trips",
-  description: "",
+  title: "Unforgettable NBA Experiences from Switzerland | Elite Sports Trips",
+  description: "Experience live NBA action with Elite Sports Trips. Travel from Switzerland to iconic U.S. cities, watch the Lakers, Warriors, Knicks, and Nets, and explore landmarks like the Golden Gate Bridge and Times Square.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-69X7YC8FSV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-69X7YC8FSV');
+          `}
+        </Script>
         <Header />
         {children}
         <Footer />
